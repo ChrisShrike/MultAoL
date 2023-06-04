d6323eb004cfc06293c51264f1a6bcf5f621d79b
NathanApple <nathanielcandra2003@gmail.com>
2023-06-04 19:08:10 +0700

    test repeat

--- AoL/script.js
+++ AoL/script.js
@@ -1,6 +1,8 @@
 let currentIndex = 0;
 // $()
+let selectedIndex = 2;
 let maxIndex = 3;
+let profileWidth = 110;
 $(document).ready(function() {
     console.log("ready!");
     $('.profile').on('click', function() {
@@ -25,10 +27,48 @@ $(document).ready(function() {
         moveProfileLeft();
     })
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function decrementIndex() {
+        if (currentIndex <= 0) {
+            currentIndex = maxIndex;
+        } else {
+            currentIndex--;
+        }
+    }
+
+    function incrementIndex() {
+        if (currentIndex >= maxIndex) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
+    }
+
     function moveProfileRight() {
         // setInterval();
-        console.log("Animate");
+        // currentIndex--;
+        decrementIndex();
+        console.log("Animate Right");
+        $('.profile').animate({
+            // opacity: ,
+            right: "+=" + 110,
+            // order: index + 1,
+            // left: "+=200",
+            // transform: 'translate(100% , 0) skew(20 deg)',
+        }, 0);
+        $('.profile').css("right", "-=110");
         for (let index = 0; index <= maxIndex; index++) {
+            // addStart($('.profile[data-id="' + index + '"]').clone());
+            // $("profile-select").prepend("AAA");
+            if ((currentIndex - index) == 0) {
+                console.log(index);
+                // addStart($('.profile[data-id="' + index + '"]').clone());
+                // removeElement($('.profile[data-id="' + index + '"]'));
+
+            }
             $('.profile[data-id="' + index + '"]').animate({
                     // opacity: ,
                     right: "-=" + 110,
@@ -37,6 +77,14 @@ $(document).ready(function() {
                     // transform: 'translate(100% , 0) skew(20 deg)',
                 }, 500,
                 function() {
+                    // console.log((currentIndex + selectedIndex) % (maxIndex + 1));
+                    if ((currentIndex - index) == 0) {
+                        // addStart($('.profile[data-id="' + index + '"]').clone());
+                        removeElement($(this));
+                    }
+                    // const sleep = ms => new Promise(r => setTimeout(r, 500));
+                    // $(this).remove();
+                    // removeElement($(this));
                     // Animation complete.
                     // console.log("Animate done");
                     // console.log(($('.profile[data-id="' + index + '"]').width() + 10));
@@ -45,9 +93,35 @@ $(document).ready(function() {
         }
     }
 
+    async function removeElement($jqueryObject) {
+        await sleep(1000);
+        $jqueryObject.remove();
+    }
+
+    function addStart($profile) {
+        $profile.css("right", 50);
+        $(".profile-select").prepend($profile);
+        console.log($profile);
+        // $(".profile-select").prepend("<p>asd</p>");
+        // $(".profile-select").append($profile);
+        // $(".profile-select").append($profile);
+    }
+
+    function addEnd($profile) {
+        $profile.css("right", (profileWidth * maxIndex) - 50);
+        $(".profile-select").append($profile);
+        console.log($profile);
+        // $(".profile-select").prepend("<p>asd</p>");
+        // $(".profile-select").append($profile);
+        // $(".profile-select").append($profile);
+    }
+
     function moveProfileLeft() {
         // setInterval();
-        console.log("Animate");
+        // currentIndex++;
+        incrementIndex();
+
+        console.log("Animate Left");
         // $('.profile').animate({
         //     // opacity: 0.95,
         //     // right: "+=200",
@@ -73,5 +147,9 @@ $(document).ready(function() {
                     // console.log(($('.profile[data-id="' + index + '"]').width() + 10));
                 });
         }
+    }
+
+    function adjustProfile() {
+
     }
 });
