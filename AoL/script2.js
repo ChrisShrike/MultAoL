784abca27aa487c98ce63035f5db8d348baa01a1
BryanEgbert <bryanegbert561@gmail.com>
2023-06-04 22:27:57 +0700

    add second iteration

---
+++ AoL/script2.js
@@ -0,0 +1,38 @@
+function loadWorkoutsSelection() {
+    var charSelect = document.getElementById("char-select")
+
+    console.log((charSelect.children.length - 1) /2);
+
+    console.log(charSelect);
+    // charSelect.children[(charSelect.children.length - 1) / 2].style = "height: 300px;  width: 300px;";
+    // charSelect.children[(charSelect.children.length - 1) / 2].children[0].style = "height: 300px; width: 300px;";
+
+    // charSelect.children.array.forEach(element => {
+        // element.onmouseenter = function(e) {
+        //     e.style = "height: 300px;  width: 300px;";
+        //     e.children[0].style = "height: 300px; width: 300px;";
+        // }
+    // });
+
+    for (let elem of charSelect.children) {
+        elem.onmouseenter = function(e) {
+            e.target.style = "height: 300px;  width: 300px;";
+            e.target.children[0].style = "height: 300px; width: 300px;";
+            // console.log(e);
+            // e.children[0].style = "height: 300px; width: 300px;";
+        }
+
+        elem.onmouseleave = function(e) {
+            e.target.style = "";
+            e.target.children[0].style = "";
+            // console.log(e);
+            // e.children[0].style = "height: 300px; width: 300px;";
+        }
+    }
+}
+
+// function focusWorkoutSelection(index) {
+//     var charSelect = document.getElementById("char-select");
+//     charSelect.children[index].style = "height: 300px;  width: 300px;";
+//     charSelect.children[index].children[0].style = "height: 300px; width: 300px;";
+// }
