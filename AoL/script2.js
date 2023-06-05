8936b01dfb779d06eeb37e2c27c58b0bc8f17783
BryanEgbert <bryanegbert561@gmail.com>
2023-06-05 09:32:22 +0700

    add animation to content

--- AoL/script2.js
+++ AoL/script2.js
@@ -1,22 +1,20 @@
+const ANIMATION_DURATION = 100;
+const IMAGES = ['img/example1.jpg', 'img/example2.jpg', 'img/example3.jpeg', 'img/example4.jpg', 'img/ex1.png', 'img/ex2.png', 'img/ban1.png']
+
 function loadWorkoutsSelection() {
-    let charSelect = document.getElementById("char-select");
+    // let charSelect = document.getElementById("char-select");
     let workoutSelect = document.getElementsByClassName("workout-select");
+    
+    for (let i = 0; i < IMAGES.length; ++i) {
+        workoutSelect[i].innerHTML = `<img src="${IMAGES[i]}" alt="workout"></img>`;
+    }
+
     let initialIndex = (workoutSelect.length - 1) / 2;
 
-    console.log(initialIndex);
-    workoutSelect[initialIndex].style = "height: 250px;  width: 300px;";
-    // charSelect.children[(charSelect.children.length - 1) / 2].children[0].style = "height: 250px;  width: 300px;";
+    let workoutInfo = document.getElementById("workout-info"); 
 
-    console.log(charSelect);
-    // charSelect.children[(charSelect.children.length - 1) / 2].style = "height: 300px;  width: 300px;";
-    // charSelect.children[(charSelect.children.length - 1) / 2].children[0].style = "height: 300px; width: 300px;";
-
-    // charSelect.children.array.forEach(element => {
-        // element.onmouseenter = function(e) {
-        //     e.style = "height: 300px;  width: 300px;";
-        //     e.children[0].style = "height: 300px; width: 300px;";
-        // }
-    // });
+    workoutSelect[initialIndex].style = "height: 100%;";
+    workoutInfo.innerHTML = `<img src="${IMAGES[initialIndex]}" class="bg-img"></img>`;
 
     nextSelectBtn = document.getElementById("next-btn");
     prevSelectBtn = document.getElementById("previous-btn");
@@ -25,46 +23,92 @@ function loadWorkoutsSelection() {
         workoutSelect[initialIndex].style = "";
         workoutSelect[initialIndex].children[0].style = "";
 
+        let prevIndex = initialIndex;
         if (initialIndex >= workoutSelect.length - 1) {
             initialIndex = 0;
+            prevIndex = workoutSelect.length - 1;
         } else {
             initialIndex++;
         }
-        workoutSelect[initialIndex].style = "height: 250px;  width: 300px;";
-        workoutSelect[initialIndex].children[0].style = "height: 250px;  width: 300px;";
-    }
+
+        workoutSelect[initialIndex].animate(
+            [
+                {height: "70%"}, 
+                {height: "80%"}, 
+                {height: "90%"}, 
+                {height: "100%"},
+            ], 
+            {duration: ANIMATION_DURATION}
+        );
+
+        workoutSelect[prevIndex].animate([
+            {height: "100%"},
+            {height: "90%"},
+            {height: "80%"},
+            {height: "70%"},
+        ], {duration: ANIMATION_DURATION});
+
+        workoutSelect[initialIndex].style = "height: 100%;";
+
+        workoutInfo.animate(
+            [
+                {opacity: 0},
+                {opacity: 0.5},
+                {opacity: 1},
+            ],
+            {duration: 300}
+        )
+        // workoutInfo.style = "opacity: 1;";
+
+        workoutInfo.innerHTML = `<img src="${IMAGES[initialIndex]}" class="bg-img"></img>`;
+
+    };
 
     prevSelectBtn.onclick = function(e) {
         workoutSelect[initialIndex].style = "";
         workoutSelect[initialIndex].children[0].style = "";
 
+        let prevIndex = initialIndex;
         if (initialIndex <= 0) {
             initialIndex = workoutSelect.length - 1;
+            prevIndex = 0;
         } else {
             initialIndex--;
         }
 
-        workoutSelect[initialIndex].style = "height: 250px;  width: 300px;";
-        workoutSelect[initialIndex].children[0].style = "height: 250px;  width: 300px;";
+        workoutSelect[initialIndex].animate(
+            [
+                {height: "70%"}, 
+                {height: "80%"}, 
+                {height: "90%"}, 
+                {height: "100%"},
+            ], 
+            {duration: ANIMATION_DURATION}
+        );
 
-    }
-    // for (let elem of charSelect.children) {
-    //     elem.onmouseenter = function(e) {
-    //         e.target.style = "height: 100%;  width: 300px;";
-    //         e.target.children[0].style = "height: 100%; width: 300px;";
-    //         document.getElementById("workout-info").innerHTML += "<h1>fdsfdsfd</h1>";
-    //     }
+        workoutSelect[prevIndex].animate([
+            {height: "100%"},
+            {height: "90%"},
+            {height: "80%"},
+            {height: "70%"},
+        ], {duration: ANIMATION_DURATION})
 
-    //     elem.onmouseleave = function(e) {
-    //         e.target.style = "";
-    //         e.target.children[0].style = "";
-    //         document.getElementById("workout-info").innerHTML = "";
-    //     }
-    // }
+        workoutSelect[initialIndex].style = "height: 100%;";
+
+        workoutInfo.innerHTML = `<p>${initialIndex}</p>`;
+        
+        workoutInfo.animate(
+            [
+                {opacity: 0},
+                {opacity: 0.5},
+                {opacity: 1},
+            ],
+            {duration: 300}
+        )
+        workoutInfo.innerHTML = `<img src="${IMAGES[initialIndex]}" class="bg-img"></img>`;
+
+
+    };
+
+
 }
-
-// function focusWorkoutSelection(index) {
-//     var charSelect = document.getElementById("char-select");
-//     charSelect.children[index].style = "height: 300px;  width: 300px;";
-//     charSelect.children[index].children[0].style = "height: 300px; width: 300px;";
-// }
