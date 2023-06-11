b5e74003510b190f8b08e0b9c951bae605f298e8
BryanEgbert <bryanegbert561@gmail.com>
2023-06-11 12:45:47 +0700

    add animation and refactor code

--- AoL/script.js
+++ AoL/script.js
@@ -47,6 +47,105 @@ $(document).ready(function() {
         }
     }
 
+    function createStartAndEndProfile() {
+        $(".profile-select").append($('.profile[data-id="' + 0 + '"]').clone());
+        $(".profile-select").prepend($('.profile[data-id="' + maxIndex + '"]').clone());
+        // jQuery.data($('.profile'), "blah", "hello");
+        // $('.profile').data('position', 'Something');
+    }
+
+    createStartAndEndProfile();
+
+    function animateRight(){
+        let triggerAnimate = true;
+        $('.profile a img').animate({
+            width: defaultWidth,
+            height: defaultHeight,
+        }, 500, "swing",
+        function() {
+            if (triggerAnimate){
+                triggerAnimate = false;
+                $('.profile').animate({
+                    right: "-=" + profileWidth,
+                }, 500, "swing",
+                function() {
+                    // console.log($(this).index());
+                    $(this).css("right", starterRight);
+                    
+                    if ($(this).index() === (selectedIndex)){
+
+                        $(this).children("a").children("img").animate({
+                            width: defaultHeight * selectedMultiplier,
+                            height: defaultWidth * selectedMultiplier,
+                        }, 500, "swing",
+                        function() {
+                            disableClick = false;
+                            console.log(disableClick);
+                            // triggerAnimate = true;
+                        });
+                    }
+    
+                    if ($(this).index() === maxIndex + 3) {
+                        removeElement($(this));
+                    }
+    
+                    if ($(this).index() === maxIndex) {
+                        addStart($(this));
+                    }
+                    
+    
+                });
+            }
+        });
+    }
+
+    function animateLeft(){
+        let triggerAnimate = true;
+        let shouldDelete = true;
+
+        $('.profile a img').animate({
+            width: defaultWidth,
+            height: defaultHeight,
+        }, 500, "swing",
+        function() {
+
+            if (triggerAnimate){
+                triggerAnimate = false;
+                $('.profile').animate({
+                    right: "+=" + profileWidth + "px",
+                }, 500, "swing",
+                function() {
+                    // console.log($(this).index());
+                    $(this).css("right", starterRight);
+                    
+                    if ($(this).index() === (selectedIndex + 1)){
+
+                        $(this).children("a").children("img").animate({
+                            width: defaultWidth * selectedMultiplier,
+                            height: defaultHeight * selectedMultiplier,
+                        }, 500, "swing",
+                        function() {
+                            disableClick = false;
+                            console.log(disableClick);
+                            // triggerAnimate = true;
+                        });
+                    }
+    
+                    if ($(this).index() === 0 && shouldDelete) {
+                        shouldDelete = false;
+                        removeElement($(this));
+                    }
+    
+                    if ($(this).index() === 1) {
+                        addEnd($(this));
+                    }
+                    
+    
+                });
+            }
+        });
+    }
+
     function moveProfileRight() {
         // setInterval();
         // currentIndex--;
--- AoL/script2.js
+++ AoL/script2.js
@@ -1,90 +1,50 @@
 const ANIMATION_DURATION = 100;
-const IMAGES = ['img/example1.jpg', 'img/example2.jpg', 'img/example3.jpeg', 'img/example4.jpg', 'img/ex1.png', 'img/ex2.png', 'img/ban1.png']
+const IMAGES = ['img/example1.jpg', 'img/example2.jpg', 'img/example3.jpeg', 'img/example4.jpg', 'img/ex1.png', 'img/ban1.png']
+
 
 function loadWorkoutsSelection() {
+    let currentIndex = 0;
     // let charSelect = document.getElementById("char-select");
     let workoutSelect = document.getElementsByClassName("workout-select");
-    
-    for (let i = 0; i < IMAGES.length; ++i) {
-        workoutSelect[i].innerHTML = `<img src="${IMAGES[i]}" alt="workout"></img>`;
-    }
-
-    let initialIndex = (workoutSelect.length - 1) / 2;
-
     let workoutInfo = document.getElementById("workout-info"); 
+    let selectedIndex = Math.ceil((workoutSelect.length - 1) / 2);
+    
+    initWorkoutSelectImages(workoutSelect, selectedIndex);
 
-    workoutSelect[initialIndex].style = "height: 100%;";
-    workoutInfo.innerHTML = `<img src="${IMAGES[initialIndex]}" class="bg-img"></img>`;
+    workoutInfo.innerHTML = `<img src="${IMAGES[selectedIndex]}" class="bg-img"></img>`;
 
-    nextSelectBtn = document.getElementById("next-btn");
-    prevSelectBtn = document.getElementById("previous-btn");
+    const NEXT_BTN = document.getElementById("next-btn");
+    const PREV_BTN = document.getElementById("previous-btn");
 
-    nextSelectBtn.onclick = function(e) {
-        workoutSelect[initialIndex].style = "";
-        workoutSelect[initialIndex].children[0].style = "";
+    NEXT_BTN.onclick = function(e) {
+        currentIndex++;
 
-        let prevIndex = initialIndex;
-        if (initialIndex >= workoutSelect.length - 1) {
-            initialIndex = 0;
-            prevIndex = workoutSelect.length - 1;
-        } else {
-            initialIndex++;
-        }
+        let prevIndex;
+        ({ prevIndex, selectedIndex } = getPrevAndNextIndex(selectedIndex, workoutSelect.length));
 
-        workoutSelect[initialIndex].animate(
-            [
-                {height: "70%"}, 
-                {height: "80%"}, 
-                {height: "90%"}, 
-                {height: "100%"},
-            ], 
-            {duration: ANIMATION_DURATION}
-        );
+        animateSlider(workoutSelect, selectedIndex, prevIndex);
+        animateContent(workoutInfo, workoutSelect[selectedIndex], (currentIndex + 1) % IMAGES.length);
 
-        workoutSelect[prevIndex].animate([
-            {height: "100%"},
-            {height: "90%"},
-            {height: "80%"},
-            {height: "70%"},
-        ], {duration: ANIMATION_DURATION});
+        workoutSelect[0].innerHTML = `<img src="${IMAGES[currentIndex % 6]}" alt="workout"></img>`
+        workoutSelect[1].innerHTML = `<img src="${IMAGES[(currentIndex + 1) % 6]}" alt="workout"></img>`
+        workoutSelect[2].innerHTML = `<img src="${IMAGES[(currentIndex + 2) % 6]}" alt="workout"></img>`
 
-        workoutSelect[initialIndex].style = "height: 100%;";
-
-        workoutInfo.animate(
-            [
-                {opacity: 0},
-                {opacity: 0.5},
-                {opacity: 1},
-            ],
-            {duration: 300}
-        )
-        // workoutInfo.style = "opacity: 1;";
-
-        workoutInfo.innerHTML = `<img src="${IMAGES[initialIndex]}" class="bg-img"></img>`;
-
+        workoutSelect[0].style = "height: 70%;";
+        workoutSelect[1].style = "height: 100%;";
+        workoutSelect[2].style = "";
     };
 
-    prevSelectBtn.onclick = function(e) {
-        workoutSelect[initialIndex].style = "";
-        workoutSelect[initialIndex].children[0].style = "";
+    PREV_BTN.onclick = function(e) {
+        workoutSelect[selectedIndex].style = "";
+        workoutSelect[selectedIndex].children[0].style = "";
 
-        let prevIndex = initialIndex;
-        if (initialIndex <= 0) {
-            initialIndex = workoutSelect.length - 1;
+        let prevIndex = selectedIndex;
+        if (selectedIndex <= 0) {
+            selectedIndex = workoutSelect.length - 1;
             prevIndex = 0;
         } else {
-            initialIndex--;
+            selectedIndex--;
         }
-
-        workoutSelect[initialIndex].animate(
-            [
-                {height: "70%"}, 
-                {height: "80%"}, 
-                {height: "90%"}, 
-                {height: "100%"},
-            ], 
-            {duration: ANIMATION_DURATION}
-        );
 
         workoutSelect[prevIndex].animate([
             {height: "100%"},
@@ -93,22 +53,87 @@ function loadWorkoutsSelection() {
             {height: "70%"},
         ], {duration: ANIMATION_DURATION})
 
-        workoutSelect[initialIndex].style = "height: 100%;";
-
-        workoutInfo.innerHTML = `<p>${initialIndex}</p>`;
-        
-        workoutInfo.animate(
+        workoutSelect[selectedIndex].animate(
             [
-                {opacity: 0},
-                {opacity: 0.5},
-                {opacity: 1},
-            ],
-            {duration: 300}
-        )
-        workoutInfo.innerHTML = `<img src="${IMAGES[initialIndex]}" class="bg-img"></img>`;
+                {height: "70%"}, 
+                {height: "80%"}, 
+                {height: "90%"}, 
+                {height: "100%"},
+            ], 
+            {duration: ANIMATION_DURATION}
+        );
 
-
+        workoutSelect[selectedIndex].style = "height: 100%;";
     };
 
 
+}
+
+function initWorkoutSelectImages(workoutSelect, selectedIndex) {
+    for (let i = 0; i < workoutSelect.length; ++i) {
+        workoutSelect[i].innerHTML = `<img src="${IMAGES[i]}" alt="workout"></img>`;
+    }
+
+    workoutSelect[selectedIndex].style = "height: 100%;";
+}
+
+function animateContent(workoutInfo, workoutSelect, selectedIndex) {
+    console.log(selectedIndex);
+
+    workoutInfo.animate(
+        [
+            { opacity: 0 },
+            { opacity: 0.5 },
+            { opacity: 1 },
+        ],
+        { duration: 300 }
+    );
+
+    workoutInfo.innerHTML = `<img src="${IMAGES[selectedIndex]}" class="bg-img"></img>`;
+    let currentImage = new Image();
+    // currentImage.src = workoutSelect.children[0].w;
+    workoutInfo.innerHTML += `<p>${workoutSelect.children[0].width}</p>`;
+}
+
+function animateSlider(workoutSelect, selectedIndex, prevIndex) {
+    workoutSelect[0].animate(
+        [
+            { transform: "translateX(7.5em)", height: "100%" },
+            { height: "90%" },
+            { height: "80%" },
+            { height: "70%" },
+        ],
+        { duration: ANIMATION_DURATION}
+    );
+
+    workoutSelect[2].animate(
+        [
+            { transform: "translateX(7.5em)", opacity: 0.2 },
+            { opacity: 0.5 },
+            { opacity: 0.7 },
+            { opacity: 1 },
+        ],
+        { duration: ANIMATION_DURATION }
+    );
+
+    workoutSelect[1].animate([
+        { transform: "translateX(7.5em)", height: "70%" },
+        { height: "80%" },
+        { height: "90%" },
+        { height: "100%" },
+    ], { duration: ANIMATION_DURATION});
+
+    workoutSelect[selectedIndex].style = "position: relative; right: 7.5em;";
+    workoutSelect[prevIndex].style = "position: relative; right: 7.35em";
+}
+
+function getPrevAndNextIndex(selectedIndex, workoutSelectLength) {
+    let prevIndex = selectedIndex;
+    if (selectedIndex >= workoutSelectLength - 1) {
+        selectedIndex = 0;
+        prevIndex = workoutSelectLength - 1;
+    } else {
+        selectedIndex++;
+    }
+    return { prevIndex, selectedIndex };
 }
